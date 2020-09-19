dbae0ee17e430b72c2f7b35c25725ea0f4b8497a
Rıdvan Altun <ridvanaltun@outlook.com>
2020-09-20 02:50:51 +0300

    feat: markdown preview

--- web-client/pages/markdown/[path].js
+++ web-client/pages/markdown/[path].js
@@ -1,39 +1,117 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import ReactMarkdown from 'react-markdown'
-import axios from 'axios'
+
+// Components
+import Box from '@material-ui/core/Box'
 
 // Custom Components
-import { Blank } from '../../components'
+import { Blank, PasswordHandling } from '../../components'
 
 // Redux
 import { useDispatch } from 'react-redux'
 import { setTitle } from '../../store/actions/appAction'
 
-const MarkdownPreview = ({ content }) => {
+// Utils
+import { useRouter } from 'next/router'
+import { apiClient, useCookie } from '../../utils'
+
+const MarkdownPreview = ({ noteInfo, path, hasPassword }) => {
   const dispatch = useDispatch()
+  const router = useRouter()
+
+  console.log(noteInfo)
+
+  // States
+  const [note, setNote] = useState(noteInfo)
+  const [handlingPassword, setHandlingPassword] = useState(hasPassword)
+  const [passwordCookie, updatePasswordCookie] = useCookie(`notes-${path}`, false)
 
   // Change App Title
+  useEffect(() => { dispatch(setTitle('Markdown Preview')) }, [])
+
+  // Handle password
   useEffect(() => {
-    dispatch(setTitle('Markdown Preview'))
+    async function fetchNoteWithPassword () {
+      if (passwordCookie) {
+        try {
+          // Fetch Note with password
+          const res = await apiClient({
+            method: 'post',
+            url: `/notes/${path}`,
+            data: {
+              password: passwordCookie
+            }
+          }, false)
+          setNote(res.data)
+          setHandlingPassword(false)
+        } catch (error) {
+          // Go to note login page
+          const redirectLocation = `/${path}/login`
+          router.push(redirectLocation)
+        }
+      } else {
+        // Go to note login page directly
+        const redirectLocation = `/${path}/login`
+        router.push(redirectLocation)
+      }
+    }
+    if (hasPassword) fetchNoteWithPassword()
   }, [])
 
-  if (content) {
-    return <ReactMarkdown source={content} />
+  if (note.text) {
+    return (
+      <Box m={10}>
+        <ReactMarkdown source={note.text} />
+      </Box>
+    )
   }
+
+  if (handlingPassword) return <PasswordHandling />
 
   return <Blank />
 }
 
 MarkdownPreview.getInitialProps = async (ctx) => {
   const { path } = ctx.query
-  const res = await axios.get('https://api.github.com/repos/vercel/next.js')
-  const data = await res.data
-  return { stars: data.stargazers_count }
+
+  try {
+    // Fetch Note
+    const res = await apiClient({
+      method: 'get',
+      url: `/notes/${path}`
+    }, false)
+
+    return { hasPassword: false, noteInfo: res.data, path }
+  } catch (error) {
+    // Handle Error
+    const noteNotFounded = error.response && error.response.status === 404
+    const noteHasPassword = error.response && error.response.status === 400
+
+    if (noteNotFounded) {
+      // Create Note
+      const res = await apiClient({
+        method: 'post',
+        url: '/notes',
+        data: {
+          path
+        }
+      }, false)
+
+      return { hasPassword: false, noteInfo: res.data, path }
+    }
+
+    if (noteHasPassword) return { hasPassword: true, noteInfo: {}, path }
+
+    // Other Errors
+    return { error }
+  }
 }
 
 MarkdownPreview.propTypes = {
-  content: PropTypes.object
+  noteInfo: PropTypes.object,
+  path: PropTypes.string,
+  hasPassword: PropTypes.bool
 }
 
 export default MarkdownPreview
