89f2fbcf50038c44a16aceee9f7af15431ccd976
Rıdvan Altun <ridvanaltun@outlook.com>
2020-09-24 18:41:44 +0300

    feat: pwa

--- web-client/next.config.js
+++ web-client/next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
   env: {
     API_BASE_URL: 'http://localhost:8000/api',
     NOTE_DEFAULT_FONT_SIZE: 18
+  },
+  pwa: {
+    dest: 'public'
   }
-}
+})
--- web-client/pages/_app.js
+++ web-client/pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import React, { useEffect } from 'react'
 import Router from 'next/router'
 import Head from 'next/head'
@@ -58,8 +59,16 @@ const MyApp = (props) => {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Head>
+          <meta charset='utf-8' />
+          <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+          <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no' />
+          <meta name='description' content='It helps you take notes, manage your notes and share your notes.' />
+          <meta name='keywords' content='note' />
           <title>Notebin</title>
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+          <link rel="manifest" href="/manifest.json" />
+          <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+          <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         </Head>
         <ThemeProvider theme={theme(darkState)}>
           <CssBaseline />
