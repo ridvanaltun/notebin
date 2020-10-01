8198a2319efc16c7b56c70443ea27214a45ce520
Rıdvan Altun <ridvanaltun@outlook.com>
2020-10-01 07:00:04 +0300

    fix(email): make verification links otp

--- server/api/generators.py
+++ server/api/generators.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
+import six
 
-class TokenGenerator(PasswordResetTokenGenerator):
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
-            user.pk + timestamp + user.is_active
+             six.text_type(user.pk) +  six.text_type(timestamp) +  six.text_type(user.is_active)
         )
 
-account_activation_token = TokenGenerator()
+
+account_activation_token = AccountActivationTokenGenerator()
