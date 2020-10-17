235b9fa1291c9e2a3fa0b7a0f5303c1e8d05524b
Rıdvan Altun <ridvanaltun@outlook.com>
2020-10-17 10:43:19 +0300

    refactor: disable pwa in development mode

--- web-client/next.config.js
+++ web-client/next.config.js
@@ -6,6 +6,11 @@ module.exports = withPWA({
     NOTE_DEFAULT_FONT_SIZE: 18
   },
   pwa: {
+    // more at https://github.com/shadowwalker/next-pwa#configuration
+    disable: process.env.NODE_ENV === 'development', // whether to disable pwa feature as a whole
+    // register: true,
+    // scope: '/app', // string - url scope for pwa, default /
+    // sw: 'service-worker.js', // service worker script file name, default to /sw.js
     dest: 'public'
   }
 })
